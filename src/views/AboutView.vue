1ac75c7c1c9542f4e8149c6e202f8438b7fd5781
hwl <2952702912@qq.com>
2024-04-09 11:25:22 +0800

    build: vue.config.js add 路径别名

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,5 +1,12 @@
 <template>
   <div class="about">
     <h1>This is an about page</h1>
+    <Hello />
   </div>
 </template>
+<script>
+import Hello from "@component/HelloWorld.vue"
+export default {
+  components: { Hello }
+}
+</script>
--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,42 @@
 const { defineConfig } = require("@vue/cli-service")
+const path = require("path")
+const resolve = (dir) => path.join(__dirname, dir)
 module.exports = defineConfig({
   transpileDependencies: true,
-  lintOnSave: false
+  lintOnSave: false,
+  // 项目的根目录访问路径
+  publicPath: "/",
+  // 打包输出文件路径
+  outputDir: "dist",
+  // 静态资源目录（js,css,img,fonts）
+  assetsDir: "assets",
+  // 用来配置跨域，启动项目端口号
+  devServer: {
+    // 自动打开浏览器
+    // open: true,
+    // 端口号
+    port: 8888,
+    // 跨域代理
+    proxy: {
+      "/api": {
+        // 目标地址
+        target: "http://www.baidu.com",
+        // 是否改变请求地址的源
+        changeOrigin: true,
+        // 是否改变请求地址的源
+        pathRewrite: {
+          "^/api": ""
+        }
+      }
+    }
+  },
+  // 路径别名
+  configureWebpack: {
+    resolve: {
+      alias: {
+        "@component": resolve("src/components"),
+        vue$: "vue/dist/vue.esm.js"
+      }
+    }
+  }
 })
